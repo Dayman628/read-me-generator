61c2c3c65f7d2078a95d0fa053a781f4c4c491c9
DAYMAN <andrew.morrow5510@gmail.com>
2020-06-01 13:28:45 -0700

    started adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,18 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "what is the name of your readme"
+        }
+    ])
+}
 
 function writeToFile(fileName, data) {
 }
