5696f3d968d9e4f69e42a44b30459b1cd8ab6bb0
DAYMAN <andrew.morrow5510@gmail.com>
2020-06-02 09:34:43 -0700

    fixed path on generatemd

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const generatemd = require("09-NodeJS\02-Homework\read-me-generator\Develop\utils\generateMarkdown.js")
+const generatemd = require("utils\generateMarkdown.js")
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
